c11cd271f4e4c5eae1f1c334fef2d8ef3d0673e8
Nicca Small <msmall318@gmail.com>
2018-02-26 09:36:44 -0500

    Utilized exposition pipe operator from magrittr to simplify code

--- inst/apps/Boots/server.R
+++ inst/apps/Boots/server.R
@@ -8,133 +8,80 @@ server <- function(input, output) {
   model <- reactive({input$model})
   horizon <- reactive({input$horizon})
   aggregate <- reactive({input$aggregate})
-
-  # Return the formula text for printing as a caption ----
-  # output$caption <- renderText({
-  #  formulaText()
-  # })
-
-    #Generate a plot 
-  # output$SummaryPlot <- renderPlot({
-  #  Troop_Data %>% dplyr::filter(Country==input$country,Branch==input$service,Year %in% seq(input$train[1],input$train[2],1)) %>% ggplot2::ggplot(data=.,aes(Year,Interpolated))+geom_line()
-  # })
-  
-  # output$ResidualPlot <- renderPlot({
-  #   Troop_Data %>% dplyr::filter(Country==input$country,Branch==input$service,Year %in% seq(input$train[1],input$train[2],1)) %>% ggplot2::ggplot(data=.,aes(Year,Interpolated))+geom_line()
-  # })
-  
-  # d <- reactive({
-  #   dist <- switch(input$aggregate,
-  #                  agg = agg,
-  #                  disagg = disagg)
-  # })
   
   output$ARIMAPlot <- renderPlot({
-    autoplot(forecast(auto.arima(zoo(x=Troop_Data %>%
-                            dplyr::filter(Country==input$country,
-                                          Branch==input$service,
-                                          Year %in% seq(input$train[1],
-                                                        input$train[2],
-                                                        1)) %>%
-                            dplyr::select(Interpolated),
-                          order.by=index(Troop_Data %>%
-                            dplyr::filter(Country==input$country,
-                                          Branch==input$service,
-                                          Year %in% seq(input$train[1],
-                                                        input$train[2],
-                                                        1)) %>%
-                            dplyr::select(Year))+input$train[1]-1)),h=input$horizon),
-             xlab="Year",
-             ylab="Number of Military Personnel")
+    Troop_Data %>%
+      dplyr::filter(Country==input$country,
+                    Branch==input$service,
+                    Year %in% seq(input$train[1],
+                                  input$train[2],
+                                  1)) %$%
+      zoo(Interpolated,Year) %>%
+      auto.arima() %>%
+      forecast(h=input$horizon) %>%
+      autoplot(xlab="Year",ylab="Number of Military Personnel")
   })
 
   output$ETSPlot <- renderPlot({
-    autoplot(forecast(ets(zoo(x=Troop_Data %>%
-                            dplyr::filter(Country==input$country,
-                                          Branch==input$service,
-                                          Year %in% seq(input$train[1],
-                                                        input$train[2],
-                                                        1)) %>%
-                            dplyr::select(Interpolated),
-                          order.by=index(Troop_Data %>%
-                                           dplyr::filter(Country==input$country,
-                                                         Branch==input$service,
-                                                         Year %in% seq(input$train[1],
-                                                                       input$train[2],
-                                                                       1)) %>% 
-                                           dplyr::select(Year))+input$train[1]-1)),h=input$horizon),
-             xlab="Year",
-             ylab="Number of Military Personnel")
+    Troop_Data %>%
+      dplyr::filter(Country==input$country,
+                    Branch==input$service,
+                    Year %in% seq(input$train[1],
+                                  input$train[2],
+                                  1)) %$%
+      zoo(Interpolated,Year) %>%
+      ets() %>%
+      forecast(h=input$horizon) %>%
+      autoplot(xlab="Year",ylab="Number of Military Personnel")
   })
   
   output$ETSsummary <- renderPrint({
-    summary(forecast(ets(zoo(x=Troop_Data %>%
-                               dplyr::filter(Country==input$country,
-                                             Branch==input$service,
-                                             Year %in% seq(input$train[1],
-                                                           input$train[2],
-                                                           1)) %>%
-                               dplyr::select(Interpolated),
-                             order.by=index(Troop_Data %>%
-                                              dplyr::filter(Country==input$country,
-                                                            Branch==input$service,
-                                                            Year %in% seq(input$train[1],
-                                                                          input$train[2],
-                                                                          1)) %>% 
-                                              dplyr::select(Year))+input$train[1]-1)),h=input$horizon))
+    Troop_Data %>%
+      dplyr::filter(Country==input$country,
+                    Branch==input$service,
+                    Year %in% seq(input$train[1],
+                                  input$train[2],
+                                  1)) %$%
+      zoo(Interpolated,Year) %>%
+      ets() %>%
+      forecast(h=input$horizon) %>%
+      summary()
   })
   
   output$ARIMAsummary <- renderPrint({
-    summary(forecast(auto.arima(zoo(x=Troop_Data %>%
-                              dplyr::filter(Country==input$country,
-                                            Branch==input$service,
-                                            Year %in% seq(input$train[1],
-                                                          input$train[2],
-                                                          1)) %>%
-                              dplyr::select(Interpolated),
-                            order.by=index(Troop_Data %>%
-                                             dplyr::filter(Country==input$country,
-                                                           Branch==input$service,
-                                                           Year %in% seq(input$train[1],
-                                                                         input$train[2],
-                                                                         1)) %>%
-                                             dplyr::select(Year))+input$train[1]-1)),h=input$horizon))
+    Troop_Data %>%
+      dplyr::filter(Country==input$country,
+                    Branch==input$service,
+                    Year %in% seq(input$train[1],
+                                  input$train[2],
+                                  1)) %$%
+      zoo(Interpolated,Year) %>%
+      auto.arima() %>%
+      forecast(h=input$horizon) %>%
+      summary()
   })
   
   output$`ETS Residuals` <- renderPlot({
-    checkresiduals(
-      ets(zoo(x=Troop_Data %>%
-                       dplyr::filter(Country==input$country,
-                                     Branch==input$service,
-                                     Year %in% seq(input$train[1],
-                                                   input$train[2],
-                                                   1)) %>%
-                       dplyr::select(Interpolated),
-                     order.by=index(Troop_Data %>%
-                                      dplyr::filter(Country==input$country,
-                                                    Branch==input$service,
-                                                    Year %in% seq(input$train[1],
-                                                                  input$train[2],
-                                                                  1)) %>%
-                                      dplyr::select(Year)))))
+    Troop_Data %>%
+      dplyr::filter(Country==input$country,
+                    Branch==input$service,
+                    Year %in% seq(input$train[1],
+                                  input$train[2],
+                                  1)) %$%
+      zoo(Interpolated,Year) %>%
+      ets() %>%
+      checkresiduals()
   })
   
   output$`ARIMA Residuals` <- renderPlot({
-    #autoplot(residuals(
-    checkresiduals(
-      auto.arima(zoo(x=Troop_Data %>%
-                       dplyr::filter(Country==input$country,
-                                     Branch==input$service,
-                                     Year %in% seq(input$train[1],
-                                                   input$train[2],
-                                                   1)) %>%
-                       dplyr::select(Interpolated),
-                     order.by=index(Troop_Data %>%
-                                      dplyr::filter(Country==input$country,
-                                                    Branch==input$service,
-                                                    Year %in% seq(input$train[1],
-                                                                  input$train[2],
-                                                                  1)) %>%
-                                      dplyr::select(Year)))))
+    Troop_Data %>%
+      dplyr::filter(Country==input$country,
+                    Branch==input$service,
+                    Year %in% seq(input$train[1],
+                                  input$train[2],
+                                  1)) %$%
+      zoo(Interpolated,Year) %>%
+      auto.arima() %>% 
+      checkresiduals
   })
 }
